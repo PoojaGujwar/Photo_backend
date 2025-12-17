441ce8edba37fc9534fba5247986be0c3ad5f991
Pooja Gujwar <gujwarpooja@gmail.com>
2025-12-17 13:36:51 +0530

    Shared route correction

--- db.connection.js
+++ db.connection.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const mongoURI = process.env.MONGODB
 
 const initializeDatabase = async()=>{
-    await mongoose.connect(mongoURI).then(()=>console.log("Connected to DB")).catch((error)=>console.error("Error while db connection"))
+    await mongoose.connect(mongoURI).then(()=>console.log("Connected to DB")).catch((error)=>console.error("Error while db connection",error))
 }
 
 module.exports = {initializeDatabase}
--- index.js
+++ index.js
@@ -102,9 +102,6 @@ app.get("/user/profile/google", verifyAccessToken, async (req, res) => {
 });
 
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
 
 app.post("/albums",async (req, res) => {
   console.log("/albums",req.cookies)
